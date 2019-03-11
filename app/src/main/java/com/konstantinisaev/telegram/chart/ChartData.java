1e7cce1746f0662e55f36e9f23e2ecda9a386341
Isaev Konstanin <isaevkonstantin1@gmail.com>
2019-03-11 16:09:06 +0300

    Parsing chart_data.json

--- app/src/main/java/com/konstantinisaev/telegram/chart/ChartData.java
+++ app/src/main/java/com/konstantinisaev/telegram/chart/ChartData.java
@@ -1,10 +1,105 @@
 package com.konstantinisaev.telegram.chart;
 
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class ChartData {
 
+	private List<ChartItem> items;
+
+	public List<ChartItem> getItems() {
+		return items;
+	}
+
+	public void setItems(List<ChartItem> items) {
+		this.items = items;
+	}
+
+	public static ChartData parseFromJson(JSONObject jsonObject){
+		List<ChartItem> chartItems = new ArrayList<>();
+		try {
+			JSONObject types = jsonObject.getJSONObject("types");
+			JSONObject names = jsonObject.getJSONObject("names");
+			JSONObject colors = jsonObject.getJSONObject("colors");
+			JSONArray columns = jsonObject.getJSONArray("columns");
+			Map<String,ChartItem> chartsItemMap = new HashMap<>();
+
+			Iterator<String> keys = types.keys();
+			while (keys.hasNext()){
+				ChartItem chartItem = new ChartItem();
+				chartItem.setPositions(new ArrayList<Long>());
+				String key = keys.next();
+				chartItem.setType(types.getString(key));
+				chartsItemMap.put(key,chartItem);
+			}
+
+			Iterator<String> namesKeys = names.keys();
+			while (namesKeys.hasNext()){
+				String key = namesKeys.next();
+				ChartItem chartItem = chartsItemMap.get(key);
+				if(chartItem != null){
+					chartItem.setTitle(names.getString(key));
+				}
+			}
+			Iterator<String> colorsKeys = colors.keys();
+			while (colorsKeys.hasNext()){
+				String key = colorsKeys.next();
+				ChartItem chartItem = chartsItemMap.get(key);
+				if(chartItem != null){
+					chartItem.setColor(colors.getString(key));
+				}
+			}
+			for (int i = 0; i < columns.length(); i++){
+				JSONArray values = columns.getJSONArray(i);
+				String currentKey = "";
+				for (int k = 0; k < values.length(); k++){
+					if(k == 0){
+						currentKey = values.getString(k);
+					}else {
+						ChartItem chartItem = chartsItemMap.get(currentKey);
+						if(chartItem != null){
+							chartItem.addPosition(values.getLong(k));
+						}
+					}
+				}
+
+
+			}
+
+			chartItems.addAll(chartsItemMap.values());
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		ChartData chartData = new ChartData();
+		chartData.setItems(chartItems);
+		return chartData;
+	}
+
+}
+
+class ChartItem {
+
+	private String type;
 	private String title;
-	private String x;
-	private String y;
+	private String color;
+	private List<Long> positions;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
 
 	public String getTitle() {
 		return title;
@@ -14,28 +109,23 @@ public class ChartData {
 		this.title = title;
 	}
 
-	public String getX() {
-		return x;
+	public String getColor() {
+		return color;
 	}
 
-	public void setX(String x) {
-		this.x = x;
+	public void setColor(String color) {
+		this.color = color;
 	}
 
-	public String getY() {
-		return y;
+	public List<Long> getPositions() {
+		return positions;
 	}
 
-	public void setY(String y) {
-		this.y = y;
+	public void setPositions(List<Long> positions) {
+		this.positions = positions;
 	}
 
-	@Override
-	public String toString() {
-		return "ChartData{" +
-			"title='" + title + '\'' +
-			", x='" + x + '\'' +
-			", y='" + y + '\'' +
-			'}';
+	public void addPosition(Long position){
+		positions.add(position);
 	}
 }
--- app/src/main/java/com/konstantinisaev/telegram/chart/MainActivity.java
+++ app/src/main/java/com/konstantinisaev/telegram/chart/MainActivity.java
@@ -2,6 +2,15 @@ package com.konstantinisaev.telegram.chart;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,41 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		findViewById(R.id.scroller).invalidate();
+		parseJson();
+	}
+
+	private void parseJson() {
+		String json = loadJSONFromAsset();
+		if(TextUtils.isEmpty(json)){
+			return;
+		}
+		try {
+			JSONArray jsonArray = new JSONArray(json);
+			for (int i = 0;i < jsonArray.length();i++){
+				JSONObject jsonObject = jsonArray.getJSONObject(i);
+				ChartData chartData = ChartData.parseFromJson(jsonObject);
+
+			}
+			Log.d(MainActivity.class.getSimpleName(),jsonArray.toString());
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private String loadJSONFromAsset() {
+		String json = null;
+		try {
+			InputStream is = this.getAssets().open("chart_data.json");
+			int size = is.available();
+			byte[] buffer = new byte[size];
+			is.read(buffer);
+			is.close();
+			json = new String(buffer, "UTF-8");
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			return null;
+		}
+		return json;
 	}
 }
